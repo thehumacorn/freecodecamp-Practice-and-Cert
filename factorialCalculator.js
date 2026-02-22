1c4f502ce85c26d2017992d578287580b81f97c6
Matt Rieser <36212696+thehumacorn@users.noreply.github.com>
2026-02-22 17:43:50 -0500

    Update factorialCalculator.js

--- factorialCalculator.js
+++ factorialCalculator.js
@@ -1,10 +1,13 @@
-const num = 18;
+const num = 5;
 const factorial = factorialCalculator(num)
 const resultMsg = `Factorial of ${num} is ${factorial}`
 function factorialCalculator(num){
   let result = 1
+  //i = 1 as i is less than or equal to num "18" iterate through all numbers
   for(let i = result; i <= num; i++){
+      // this will multiply the previous number and assign it to the result until i is greater than num
       result *= i
+      console.log(`${result} <- ${i}`)
   }
   return result
 }
